33af36e01c19b6433c038e7d561a20d8c7035bac
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-03-07 15:37:07 +0300

    Update Trash, Weapon
    Добавил скрипт для мусора и обновил оружие

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public static Transform t;
+
+    void Start()
+    {
+    	t = transform;
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public enum firsec {first, second};
+    public enum modes {single, semiAutomatic, auto}
     [Header("Main")]
     [SerializeField] string name;
     [SerializeField] string desc;
@@ -19,7 +20,52 @@ public class Weapon : MonoBehaviour
     [SerializeField] int autoDelay;
     [Header("Bullet")]
     [SerializeField] float speed;
-    [SerializeField] GameObject muzle;
+    [SerializeField] float scatter;
+    [SerializeField] float recoil; 
+    [SerializeField] Transform muzle;
     [SerializeField] int HMBbSiSAMpC;
-    // How Many Bullets be Shooted in Semi Automatic Mode per Click 
+    // How Many Bullets be Shooted in Semi Automatic Mode per Click
+    [Header("Magazine")]
+    [SerializeField] Transform joint;
+    [SerializeField] GameObject magazine;
+    [SerializeField] int maxAmmo;
+    [SerializeField] int[] magazines;
+    [Header("Status")]
+    [SerializeField] modes mode;
+    [SerializeField] bool isAiming;
+    [SerializeField] bool isRealoding;
+    [SerializeField] int currentMagazine;
+
+    void PickMag(){}
+    void DropMag(){}
+
+    public void Reload()
+    {
+        isRealoding = true;
+        if(magazines[currentMagazine] == 0)
+            DropMag();
+        int maxMag = -1;
+        for(int i = 0; i < magazines.Length; i++)
+            if(magazines[i] > maxMag && magazines[i] != 0)
+                maxMag = i;
+        if(maxMag == -1)
+            Debug.Log("Magazines is Empty");
+        else
+        {
+            currentMagazine = maxMag;
+            PickMag();
+        }
+        isRealoding = false;
+    }
+
+    public void FillMag()
+    {
+        for(int i = 0; i < magazines.Length; i++)
+            magazines[i] = maxAmmo;
+    }
+
+    void Start()
+    {
+        FillMag();
+    }
 }
