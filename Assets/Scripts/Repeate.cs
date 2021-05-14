c0f1036d553941823326144b07c42188ca2a8446
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-05-14 13:24:04 +0300

    ...

---
+++ Assets/Scripts/Repeate.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+//Created by Emerald Powder
+
+[ExecuteAlways]
+[SelectionBase]
+public class Repeate : MonoBehaviour
+{
+    public Vector3 offset = Vector3.forward;
+
+    [Min(1)] public int count = 1;
+
+    private void Start()
+    {
+        if (Application.IsPlaying(this))
+        {
+            CorrectChildCount();
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        CorrectChildCount();
+    }
+
+    private void CorrectChildCount()
+    {
+        count = Mathf.Clamp(Count, 1, 1000);
+
+        if (transform.childCount < Count)
+        {
+            for (int i = transform.childCount; i < count; i++)
+            {
+                Transform instantiate = Instantiate(transform.GetChild(0), transform);
+                instantiate.Translate(offset * i);
+            }
+        }
+        else if (count < transform.childCount)
+        {
+            for (int i = transform.childCount - 1; i >= count; i--)
+            {
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
